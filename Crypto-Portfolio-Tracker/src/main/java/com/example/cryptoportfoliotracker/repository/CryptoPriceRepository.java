4e511a2c025dc075ab9996c17264de2e6bf7d885
Ragulsankar04 <ragulsankar2004@gmail.com>
2025-05-28 17:28:16 +0530

    Deleted Unused Packages

--- Crypto-Portfolio-Tracker/src/main/java/com/example/cryptoportfoliotracker/repository/CryptoPriceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cryptoportfoliotracker.repository;
-
-import com.example.cryptoportfoliotracker.entity.CryptoPrice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CryptoPriceRepository extends  JpaRepository<CryptoPrice, String> {
-}
